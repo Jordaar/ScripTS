439ef8cf3b1ce6a9cd21837fca511496aa99b7e5
JAGUARAVI <47686625+JAGUARAVI@users.noreply.github.com>
2021-05-05 17:39:13 +0530

    + Console in eval + Cosmetic changes

--- Src/Commands/Utilities/eval.js
+++ Src/Commands/Utilities/eval.js
@@ -1,7 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 const safeEval = require('notevil');
 const clean = text => ((typeof text === "string") ? text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)) : text);
-
 module.exports = {
     name: "eval",
     category: "Utilities",
@@ -12,17 +11,27 @@ module.exports = {
 }
 
 async function execute(client, message, args, text, instance) {
-    if (!args[0]) instance.send(message, instance.embed("Please re-run the command along with the code to evaluate!"), "embed")
+    if (!args[0]) return instance.send(message, instance.embed("Please re-run the command along with the code to evaluate!",'error'), "embed")
 
     const startTime = Date.now();
     let output;
     let status = true;
+    let logs = [];
+
+    const console = {
+        log: (t) => {logs.push(t); return;},
+        error: (t) => {logs.push(t); return;},
+        warn: (t) => {logs.push(t); return;},
+    };
+
     try {
         const code = text;
-        let evaled = safeEval(code);
+        let evaled = safeEval(code,{console});
         if (evaled instanceof Promise) evaled = await evaled;
         else if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
-        output = clean(evaled);
+        evaled = clean(evaled);
+        if(!evaled || evaled == 'undefined') evaled = logs.map(p => typeof p !== "string" ? require("util").inspect(p) : p).join('\n');
+        output = evaled;
     } catch (err) {
         status = false;
         output = err.toString();
@@ -31,6 +40,6 @@ async function execute(client, message, args, text, instance) {
         .setAuthor("Evaluate JS Code", status ? "https://iili.io/BxVFZF.png" : "https://iili.io/BxV3j1.png")
         .setColor(status ? instance.config.static.color.success : instance.config.static.color.error)
         .setDescription(`\`\`\`js\n${output}\n\`\`\``)
-        .setFooter(`Time Taken − ${Math.abs(Date.now() - startTime)} ms`)
+        .setFooter(`Time Taken − ${Math.abs(Date.now() - startTime)} ms`,message.author.avatarURL({dynamic: true}))
     instance.send(message, embed, "embed")
 }
