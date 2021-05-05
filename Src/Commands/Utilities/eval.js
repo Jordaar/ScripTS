774bb94ab72c42791365d2c6769d6b3d8da8c9cb
JAGUARAVI <47686625+JAGUARAVI@users.noreply.github.com>
2021-05-05 21:38:26 +0530

    Hmmm

--- Src/Commands/Utilities/eval.js
+++ Src/Commands/Utilities/eval.js
@@ -1,6 +1,20 @@
 const { MessageEmbed } = require('discord.js');
 const safeEval = require('notevil');
 const clean = text => ((typeof text === "string") ? text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203)) : text);
+
+const chunk = (array, chunkSize = 2000) => {
+    if (!Array.isArray(array)) return array;
+    return array.reduce((previous, current) => {
+        let chunk;
+        if (!previous.length || previous[previous.length - 1].length === chunkSize) {
+            chunk = [];
+            previous.push(chunk);
+        } else chunk = previous[previous.length - 1];
+        chunk.push(current);
+        return previous;
+    }, []);
+}
+
 module.exports = {
     name: "eval",
     category: "Utilities",
@@ -19,7 +33,7 @@ async function execute(client, message, args, text, instance) {
     let status = true;
     let logs = [];
 
-    const console = {
+    const psuedoConsole = {
         log: (t) => {logs.push(t); return;},
         error: (t) => {logs.push(t); return;},
         warn: (t) => {logs.push(t); return;},
@@ -27,7 +41,7 @@ async function execute(client, message, args, text, instance) {
 
     try {
         const code = text;
-        let evaled = safeEval(code,{console});
+        let evaled = safeEval(code,{console: psuedoConsole});
         if (evaled instanceof Promise) evaled = await evaled;
         else if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
         evaled = clean(evaled);
@@ -37,10 +51,18 @@ async function execute(client, message, args, text, instance) {
         status = false;
         output = err.toString();
     }
-    const embed = new MessageEmbed()
+
+    const endTime = Date.now();
+    let chunks = chunk(output.split(''));
+
+    let embeds = chunks.map((text,index) => {
+        text = text.join('');
+        return new MessageEmbed()
         .setAuthor("Evaluate JS Code", status ? "https://iili.io/BxVFZF.png" : "https://iili.io/BxV3j1.png")
         .setColor(status ? instance.config.static.color.success : instance.config.static.color.error)
-        .setDescription(`\`\`\`js\n${output}\n\`\`\``)
-        .setFooter(`Time Taken − ${Math.abs(Date.now() - startTime)} ms`,message.author.avatarURL({dynamic: true}))
-    instance.send(message, embed, "embed")
+        .setDescription(`\`\`\`js\n${text}\n\`\`\``)
+        .setFooter(`Time Taken − ${Math.abs(endTime - startTime)} ms`,message.author.avatarURL({dynamic: true}))
+    })
+
+    return embeds.length > 1 ? instance.paginate(message, embeds) : instance.send(message, embeds[0], 'embed');    
 }
